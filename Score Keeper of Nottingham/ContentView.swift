5e60f4e0bcad17c4c92b356bb5f2279de2b193f7
Drake Morin <drake.lariviere@gmail.com>
2020-09-03 20:30:06 -0400

    Initial Commit

---
+++ Score Keeper of Nottingham/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Score Keeper of Nottingham
+//
+//  Created by Drake Morin on 2020-09-03.
+//  Copyright © 2020 Drake Morin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Score Keeper of NottinghamTests/Score_Keeper_of_NottinghamTests.swift
@@ -0,0 +1,34 @@
+//
+//  Score_Keeper_of_NottinghamTests.swift
+//  Score Keeper of NottinghamTests
+//
+//  Created by Drake Morin on 2020-09-03.
+//  Copyright © 2020 Drake Morin. All rights reserved.
+//
+
+import XCTest
+@testable import Score_Keeper_of_Nottingham
+
+class Score_Keeper_of_NottinghamTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
